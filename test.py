0a40edd9f564d3e694e8d9047c6b3b9c2d93547b
Gaurav <gaurav.chaturvedi@indiamart.com>
2018-09-10 17:58:08 +0530

    testing

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np
-im =cv2.imread("images/test2.jpg")
-resize_img = cv2.resize(im ,(28,28))
-blur = cv2.pyrMeanShiftFiltering(im,21,51)
+im =cv2.imread("images/test.jpg")
+
+
+scale_percent = 60 # percent of original size
+width = int(im.shape[1] * scale_percent / 100)
+height = int(im.shape[0] * scale_percent / 100)
+dim = (width, height)
+# resize image
+im = cv2.resize(im, dim, interpolation = cv2.INTER_AREA)
+
+
+blur = cv2.pyrMeanShiftFiltering(im,111,151)
 gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
-ret ,threshold = cv2.threshold(gray,0,255,cv2.THRESH_BINARY,cv2.THRESH_OTSU)
+ret ,threshold = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 _,cnts,_ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
